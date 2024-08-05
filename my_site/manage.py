aca39ba49988f1e200f8910cf4c739d10890f23d
asci-dev <104984373+2oo0k@users.noreply.github.com>
2024-08-05 22:58:45 +0300

    recreate first app

--- my_site/manage.py
+++ my_site/manage.py
@@ -1,4 +1,4 @@
-#!/nix/store/l014xp1qxdl6gim3zc0jv3mpxhbp346s-python3-3.12.4/bin/python
+#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmanager.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_site.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- taskmanager/tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'tasks'
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='tasks/home.html'), name='home'),
-    path('help/', TemplateView.as_view(template_name='tasks/help.html'), name='help'),
-]
