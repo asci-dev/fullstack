bd7f3ad6dad8093192d1c611beeb59a5844f482b
asci-dev <104984373+2oo0k@users.noreply.github.com>
2024-08-07 07:37:14 +0300

    dynamic views

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
-
 # Create your views here.
-def simple_view(request):
-    return HttpResponse("Simple view")
+
+articles = {
+    'sports': 'Sports Page',
+    'finance': 'Finance Page',
+    'politics': 'Politics Page'
+}
+
+def news_view(request, topic):
+    return HttpResponse(articles[topic])
+
+def add_view(request, num1, num2):
+    add_result = num1 + num2
+    result = f"{num1} + {num2} = {add_result}"
+    return HttpResponse(str(result))
