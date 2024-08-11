636a23934c14b1bcd005846db17d0ee8b4a23995
asci-dev <104984373+2oo0k@users.noreply.github.com>
2024-08-11 08:17:40 +0300

    connect view to template

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -3,21 +3,5 @@ from django.http.response import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
 # Create your views here.
 
-articles = {
-    'sports': 'Sports Page',
-    'finance': 'Finance Page',
-    'politics': 'Politics Page'
-}
-
-def news_view(request, topic):
-    try:
-        result = articles[topic]
-        return HttpResponse(articles[topic])
-    except:
-        raise Http404("404 GENERIC ERROR")
-
-def num_page_view(request, num_page):
-    topic_list = list(articles.keys())
-    topic = topic_list[num_page]
-
-    return HttpResponseRedirect(reverse('topic-page', args = [topic]))
+def simple_view(request):
+    return render(request, 'first_app/example.html')
